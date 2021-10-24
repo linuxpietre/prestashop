861b41a32de3d65a316e9e56dd55409067fd5f63
linuxpietre <93061793+linuxpietre@users.noreply.github.com>
2021-10-24 23:55:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,18 @@ sudo chmod -R 755 /var/www/prestashop
 > /etc/apache2/sites-available/prestashop.conf
 cat <<+ >> /etc/apache2/sites-available/prestashop.conf
 <VirtualHost *:80>
- ServerAdmin admin@esempio.com
- ServerName esempio.com
- ServerAlias www.esempio.com
- DocumentRoot /var/www/prestashop
- ErrorLog ${APACHE_LOG_DIR}/error.log
- CustomLog ${APACHE_LOG_DIR}/access.log combined
- </VirtualHost>
- +
- sudo a2ensite prestashop.conf
- sudo apache2ctl configtest
- sudo systemctl restart apache2
- sudo apt install mysql-server
- sudo apt install php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
- sudo service apache2 restart
- cd /var/www/prestashop
- 
+ServerAdmin admin@esempio.com
+ServerName esempio.com
+ServerAlias www.esempio.com
+DocumentRoot /var/www/prestashop
+ErrorLog ${APACHE_LOG_DIR}/error.log
+CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
++
+sudo a2ensite prestashop.conf
+sudo apache2ctl configtest
+sudo systemctl restart apache2
+sudo apt install mysql-server
+sudo apt install php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+sudo service apache2 restart
+cd /var/www/prestashop
